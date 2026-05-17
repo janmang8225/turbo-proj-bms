a8b2f54b9b0df6eced0981f9a79fda23e70f6412
janmang8225 <janmangmandaviya@gmail.com>
2026-05-17 11:00:45 +0530

    initialized with: prisma

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+// const prisma = new PrismaClient({ adapter }); 
+
+// to export:
+// ==========
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// ==========
+
+// export const prisma = new PrismaClient();
+export * from "../generated/prisma/client"
+
